45f094a6ab9598f6d8ef07a775ec8482c21c0ade
THANDA <powerwolf.tj@gmail.com>
2023-08-24 20:44:19 -0700

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void first(void)_attribut_((constructor));
+
+/**
+ * first - prints a sentence befor the main.
+ * function is executed.
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <list.h>
+
+/**
+ * add_node - add a new node to the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to be added in the node
+ *
+ * return: the address of the new element, or returns NULL if fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsiged int len = 0;
+
+	whle (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if(!new)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
